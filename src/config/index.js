75ece982cc273af7340508a37430c6740b1b397c
Saravanan G <SGoffl1603@gmail.com>
2018-01-27 12:35:19 +0530

    Initial commit

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ export default {
   // "port": 3005,
   // "mongoUrl": "mongodb://localhost:27017/chat-api",
   "port": process.env.PORT,
-  "mongoUrl": process.env.MONGODB_URI,
+  "mongoUrl": "mongodb://saravanan:123456>@ds117148.mlab.com:17148/socketchat",
   "bodyLimit": "100kb"
 }
