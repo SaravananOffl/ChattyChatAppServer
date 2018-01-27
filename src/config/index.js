58db7afa6dd60d4a7b5147221817b45898d512dc
Saravanan G <SGoffl1603@gmail.com>
2018-01-27 13:08:13 +0530

    rectified

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ export default {
   // "port": 3005,
   // "mongoUrl": "mongodb://localhost:27017/chat-api",
   "port": process.env.PORT,
-  "mongoUrl": "mongodb://saravanan:123456>@ds117148.mlab.com:17148/socketchat",
+  "mongoUrl": "mongodb://saravanan:123456@ds117148.mlab.com:17148/socketchat",
   "bodyLimit": "100kb"
 }
